b0e9e3ea672904dc3a90ec7e5d291ad2042ad2fc
Keshav Motwani <keshavmot2@gmail.com>
2021-02-18 10:54:22 -0500

    update test

--- scripts/test.R
+++ scripts/test.R
@@ -2,7 +2,7 @@ library(MultiLORS)
 
 set.seed(1)
 
-n_k = rep(1000, 3)
+n_k = rep(500, 3)
 p = 50
 q = 10
 theta = rep(100, 3)
@@ -31,31 +31,40 @@ E_test = simulate_E_list(n_k, Sigma)
 Y_test = compute_Y_list(X_test, Beta, L_test, E_test)
 indices_test = list(1:q, 1:q, 1:q)
 
-print(system.time({MultiLORS_fit = MultiLORS(Y, X, indices, Y_val, X_val, indices_val, verbose = 0, n_iter = 100, n_cores = 4, early_stopping = FALSE)}))
+print(system.time({MultiLORS_fit = MultiLORS(Y, X, indices, Y_val, X_val, indices_val, verbose = 1, n_iter = 250, n_cores = 1, early_stopping = FALSE)}))
 print(MultiLORS_fit$tuning$validation$n_iter)
 glmnet_fit = fit_glmnet(Y, X, indices, Y_val, X_val, indices_val)
 
 best_indices_MultiLORS = which_min(MultiLORS_fit$tuning$validation$SSE)
 best_index_glmnet = which_min(glmnet_fit$tuning$validation$SSE)
-sum((Beta[-1, ] - as.matrix(MultiLORS_fit$model_fits[[best_indices_MultiLORS[1]]][[best_indices_MultiLORS[2]]]$Beta)[-1, ]) ^ 2)
-sum((Beta[-1, ] - as.matrix(glmnet_fit$model_fits[[best_index_glmnet]]$Beta)[-1, ]) ^ 2)
+
+print(best_indices_MultiLORS)
 
 MultiLORS_Beta_hat = MultiLORS_fit$model_fits[[best_indices_MultiLORS[1]]][[best_indices_MultiLORS[2]]]$Beta
 glmnet_Beta_hat = glmnet_fit$model_fits[[best_index_glmnet]]$Beta
 
-compute_correlation(Y_test, X_test, indices_test, MultiLORS_Beta_hat)
-compute_R2(Y_test, X_test, indices_test, Y, indices, MultiLORS_Beta_hat)
-compute_SSE(Y_test, X_test, indices_test, MultiLORS_Beta_hat)
+print(MultiLORS_fit$model_fits[[5]][[5]]$Beta[1:5, 1:5])
 
-compute_avg_R2(Y_test, X_test, indices_test, Y, indices, MultiLORS_Beta_hat)
-compute_avg_R2(Y_test, X_test, indices_test, Y, indices, glmnet_Beta_hat)
-compute_avg_correlation(Y_test, X_test, indices_test, MultiLORS_Beta_hat)
-compute_avg_correlation(Y_test, X_test, indices_test, glmnet_Beta_hat)
+print(MultiLORS_Beta_hat[1:5, 1:5])
 
-MultiLORS_performance = list("R2" = compute_R2(Y_test, X_test, indices_test, Y, indices, MultiLORS_Beta_hat),
-                             "rho" = compute_correlation(Y_test, X_test, indices_test, MultiLORS_Beta_hat))
-plot_actual_vs_predicted(Y_test, X_test, indices_test, MultiLORS_Beta_hat, MultiLORS_performance)
+sum((Beta[-1, ] - as.matrix(MultiLORS_Beta_hat)[-1, ]) ^ 2)
+sum((Beta[-1, ] - as.matrix(glmnet_Beta_hat)[-1, ]) ^ 2)
 
-glmnet_performance = list("R2" = compute_R2(Y_test, X_test, indices_test, Y, indices, glmnet_Beta_hat),
-                             "rho" = compute_correlation(Y_test, X_test, indices_test, glmnet_Beta_hat))
-plot_actual_vs_predicted(Y_test, X_test, indices_test, glmnet_Beta_hat, glmnet_performance)
+
+#
+# compute_correlation(Y_test, X_test, indices_test, MultiLORS_Beta_hat)
+# compute_R2(Y_test, X_test, indices_test, Y, indices, MultiLORS_Beta_hat)
+# compute_SSE(Y_test, X_test, indices_test, MultiLORS_Beta_hat)
+#
+# compute_avg_R2(Y_test, X_test, indices_test, Y, indices, MultiLORS_Beta_hat)
+# compute_avg_R2(Y_test, X_test, indices_test, Y, indices, glmnet_Beta_hat)
+# compute_avg_correlation(Y_test, X_test, indices_test, MultiLORS_Beta_hat)
+# compute_avg_correlation(Y_test, X_test, indices_test, glmnet_Beta_hat)
+#
+# MultiLORS_performance = list("R2" = compute_R2(Y_test, X_test, indices_test, Y, indices, MultiLORS_Beta_hat),
+#                              "rho" = compute_correlation(Y_test, X_test, indices_test, MultiLORS_Beta_hat))
+# plot_actual_vs_predicted(Y_test, X_test, indices_test, MultiLORS_Beta_hat, MultiLORS_performance)
+#
+# glmnet_performance = list("R2" = compute_R2(Y_test, X_test, indices_test, Y, indices, glmnet_Beta_hat),
+#                              "rho" = compute_correlation(Y_test, X_test, indices_test, glmnet_Beta_hat))
+# plot_actual_vs_predicted(Y_test, X_test, indices_test, glmnet_Beta_hat, glmnet_performance)
